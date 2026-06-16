4be919cd010cb2ef2384b083aea38e89944d70b6
PedroHKV <piphkv@gmail.com>
2026-06-16 00:44:48 -0300

    primeiro sucesso no nginx

--- LBM/Componentes/IoT1/TMP_UMD.cpp
+++ LBM/Componentes/IoT1/TMP_UMD.cpp
@@ -8,7 +8,7 @@
 #define PERIODO_ENVIO 3000
 #define SSD "Laboratorios_Instituição_EXT"
 #define SENHA "96252666"
-#define SERVIDOR String("http://192.168.15.100")
+#define SERVIDOR String("http://192.168.1.101")
 
 DHT dht(PORTA_DHT, DHT11);
 
